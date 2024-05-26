a64d19daecbadedf05481b9f50ce6baabd022ba7
quange00407 <151482856+quange00407@users.noreply.github.com>
2024-05-26 21:37:33 +0800

    apool-lstar.sh

--- apool-lstar.sh
+++ apool-lstar.sh
@@ -1,7 +1,16 @@
-echo "# boke" >> README.md 
-git init 
-git add README.md 
-git commit -m "首次提交" 
-git branch -M main 
-git remote add origin https://github.com/quange00407/boke.git
- git push -u origin main
+cd /
+rm -rf apoolminer_linux_autoupdate_v1.4.1.tar
+rm -rf apoolminer_linux_autoupdate_v1.4.1
+wget https://github.com/apool-io/apoolminer/releases/download/v1.4.1/apoolminer_linux_autoupdate_v1.4.1.tar
+tar -xvf apoolminer_linux_autoupdate_v1.4.1.tar
+cd /apoolminer_linux_autoupdate_v1.4.1
+variable1=$(< /dev/urandom tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
+sed -i "s/test_accountname/CP_c4uaciihm3 --worker LStarD${variable1}/g" ./run_qubic_cpu.sh
+sed -i "s/CP_c4uaciihm3/CP_c4uaciihm3 --worker LStarD ${variable1}/g" ./run_qubic_cpu.sh
+cd /etc/init.d
+echo "正在部署..." > qubic.sh
+chmod a+x qubic.sh
+update-rc.d qubic.sh defaults
+cd /apoolminer_linux_autoupdate_v1.4.1
+nohup ./run_qubic_cpu.sh &
+ps -ef | grep apool
