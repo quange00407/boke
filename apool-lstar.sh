a48e76816fcfbfe1d7d14a7fa6eeaba10782f5e2
quange00407 <151482856+quange00407@users.noreply.github.com>
2024-05-26 21:33:44 +0800

    apool-lstar.sh

---
+++ apool-lstar.sh
@@ -0,0 +1,7 @@
+echo "# boke" >> README.md 
+git init 
+git add README.md 
+git commit -m "首次提交" 
+git branch -M main 
+git remote add origin https://github.com/quange00407/boke.git
+ git push -u origin main
